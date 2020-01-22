9245b70fa818dde1a22186723c2a37da28f8dac3
avinash <avinash.aryal@mavs.uta.edu>
2020-01-22 14:55:12 -0600

    Code using getline

--- P01/bonus/hello.cpp
+++ P01/bonus/hello.cpp
@@ -1,14 +1,16 @@
 /*Avinash Aryal 1001727418*/
 #include <iostream>
-
+#include <string.h>
 using namespace std;
 
 int main()
 {
 	string name;	
 	cout<<"What is your name?"<<endl;
-	cin>>name;
+	//cin>>name;
+	getline(cin,name);
 	cout<<"Hello "<<name<<endl;
+	return 0;
 
 }
 
