fadf49f3a8aa66fe312c422c94860e201d6eb97e
avinash <avinash.aryal@mavs.uta.edu>
2020-01-22 14:49:09 -0600

    Input using cin. cin terminates at spaces

---
+++ P01/bonus/hello.cpp
@@ -0,0 +1,14 @@
+/*Avinash Aryal 1001727418*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;	
+	cout<<"What is your name?"<<endl;
+	cin>>name;
+	cout<<"Hello "<<name<<endl;
+
+}
+
