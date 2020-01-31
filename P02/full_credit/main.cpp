de6feafa0db09964b745672e99a1eabe5bb241eb
avi <avinash.aryal@mavs.uta.edu>
2020-01-31 13:18:09 -0600

    third try

---
+++ P02/full_credit/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "color.h"
+
+int main()
+{
+	int red, green, blue;
+	Color test{25,100,80};
+	std::cout<<test.colorize("Red")<<std::endl;
+	std::cout<<"Please enter the value of the red, green and blue color"<<std::endl;
+	std::cin>>red>>green>>blue;
+	Color user{red,green,blue};
+	std::cout<<user.colorize(user.to_string())<<std::endl;
+	return 0;
+
+}
+
