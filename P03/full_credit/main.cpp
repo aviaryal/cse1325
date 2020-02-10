96d75a04d3dcffe36fb910f7aa450e852610d098
aviaryal <58341812+aviaryal@users.noreply.github.com>
2020-02-10 14:54:44 -0600

    Update main.cpp

--- P03/full_credit/main.cpp
+++ P03/full_credit/main.cpp
@@ -5,8 +5,8 @@ int main()
 {
 	Color reset;
 	Color uta_blue{0,100,177};
-    Color uta_orange{245,128,38};
-    Color maroon{93,23,37};
+    	Color uta_orange{245,128,38};
+    	Color maroon{93,23,37};
 	std::cout<<uta_blue<<"uta_blue"<<reset<<std::endl;
 	std::cout<<uta_orange<<"uta orange"<<reset<<std::endl;
 	std::cout<<maroon<<"maroon"<<reset<<std::endl;
