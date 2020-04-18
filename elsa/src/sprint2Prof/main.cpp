bf4e2a465fb82d1503548184644060c2d6e29fa9
aviaryal <avinash.aryal@mavs.uta.edu>
2020-04-18 12:38:05 -0500

    Remove duplicated directory

--- elsa/src/sprint2Prof/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtkmm.h>
-#include "mainwin.h"
-
-int main (int argc, char *argv[]) {
-    auto app = Gtk::Application::create(argc, argv, "edu.uta.cse1325.elsa.v0_2_0");
-    Mainwin win;
-    return app->run(win);
-}
-
